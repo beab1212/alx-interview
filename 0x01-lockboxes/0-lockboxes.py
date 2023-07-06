0e571e8437207572398ed73e64d5e9c2522f34f5
Mr-robot <hackingtest017@gmail.com>
2023-07-06 13:36:54 -0400

    add check points

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,19 +8,11 @@ def canUnlockAll(boxes):
         return False
     if len(boxes) == 1:
         return True
-    unlocked_box = [boxes[0]]
-    keys = [key for key in boxes[0]]
-
-    for i in range(0, len(boxes)):
-        for key_ in keys:
-            if key_ < len(boxes):
-                if boxes[key_] not in unlocked_box:
-                    for key__ in boxes[key_]:
-                        if key__ not in keys:
-                            keys.append(key__)
-                    unlocked_box.append(boxes[key_])
-
-    if len(boxes) == len(unlocked_box):
+    keys = [0]
+    for key in keys:
+        for new_key in boxes[key]:
+            if new_key not in keys and new_key < len(boxes):
+                keys.append(new_key)
+    if len(keys) == len(boxes):
         return True
-    else:
-        return False
+    return False
