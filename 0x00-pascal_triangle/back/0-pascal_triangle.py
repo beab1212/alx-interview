404a0b03fd7888b9e6dd7624afd9cf3f35e36d94
Mr-robot <hackingtest017@gmail.com>
2023-07-06 13:29:18 -0400

    add boxes length check

---
+++ 0x00-pascal_triangle/back/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    This function returns a list of lists of integers representing
+    the pascal's triangle of n
+    """
+    result = []
+
+    if n <= 0:
+        return result
+    for i in range(n):
+        row = []
+
+        for j in range(i+1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                row.append(result[i-1][j-1] + result[i-1][j])
+        result.append(row)
+    return result
