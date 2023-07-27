c815931eb3b721a9b9c20f2d881eeecb3591fcbf
Mr-robot <hackingtest017@gmail.com>
2023-07-27 16:00:24 -0400

    hot fix

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" """
+def validUTF8(data):
+    """validUTF8
+    	Args:
+    		data (list): 
+    	Returns:
+    		True if data is valid UTF-8 encoding else False
+    """
+    status = None
+
+    for item in data:
+        binary = bin(item).replace('0b', '').rjust(8, '0')[-8:]
+        if status == None:
+            if binary.startswith('110'):
+                status = 1
+            if binary.startswith('1110'):
+                status = 2
+            if binary.startswith('11110'):
+                status = 3
+            if binary.startswith('10'):
+                return False
+        else:
+            if not binary.startswith('10'):
+                return False
+            status -= 1
+
+    if status != None:
+        return False
+
+    return True
