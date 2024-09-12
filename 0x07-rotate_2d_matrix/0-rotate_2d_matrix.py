5044271bfe69d071a18bae5307fcf335415f6ef3
Mr-robot <hackingtest017@gmail.com>
2024-09-12 07:43:44 +0300

    2d rotation

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Main file
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotate matrix it 90 degrees
+    """
+    duplicate = matrix[:]
+
+    for i in range(len(matrix)):
+        column = [row[i] for row in duplicate]
+        matrix[i] = column[::-1]
